8a10c90a12518b51b68cb3ad4c350dafb2d01d50
Ravin30 <ravindrapratap1030@gmail.com>
2021-10-08 17:02:54 +0530

    webchat-appConfig.sample.js file updated

--- public/assets/webchat-appConfig.sample.js
+++ public/assets/webchat-appConfig.sample.js
@@ -1,9 +1,58 @@
 var appConfig = {
-    //  change the your AccountSid
-    accountSid: "AC...",
-    // change to your Flex Flow SID
-    flexFlowSid: "FO...",
-    colorTheme: {
-        overrides: brandedColors
-    }
+  //  change the your AccountSid
+  accountSid: 'AC...',
+  // change to your Flex Flow SID
+  flexFlowSid: 'FO...',
+  // colorTheme: {
+  //     overrides: brandedColors
+  // }
+  context: {
+    fileAttachment: true
+  },
+
+  startEngagementOnInit: false,
+  preEngagementConfig: {
+    description: 'Please provide your information to stay connected',
+    fields: [
+      {
+        label: 'Name?',
+        type: 'InputItem',
+        attributes: {
+          name: 'friendlyName',
+          type: 'text',
+          placeholder: 'Enter your name',
+          required: false,
+          readOnly: false
+        }
+      },
+      {
+        label: 'Email?',
+        type: 'InputItem',
+        attributes: {
+          name: 'email',
+          type: 'email',
+          placeholder: 'Enter yout email',
+          required: false,
+          readOnly: false
+        }
+      }
+
+      // {
+      //     label: "What is your question?",
+      //     type: "TextareaItem",
+      //     attributes: {
+      //         name: "question",
+      //         type: "text",
+      //         placeholder: "Type your question here",
+      //         required: false,
+      //         rows: 5,
+      //         value: "My awesome question",
+      //         readOnly: false
+      //     }
+      // }
+    ],
+    footerLabel:
+      '(the information provided herein will be kept private,and will not be used for any other purpose than responding the queries)',
+    submitLabel: "Ok Let's Go!"
+  }
 }
